1fd6c98a081cbb9d95aa2f3b07d77fb3ad12fed7
Robin Oosterik <robin.oosterik@pescheck.nl>
2024-12-04 18:47:53 +0100

    fonts, pinia, tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,17 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/image', '@nuxt/eslint'],
-
+  modules: [
+    '@nuxt/image',
+    '@nuxt/eslint',
+    '@pinia/nuxt',
+    '@nuxt/fonts',
+    '@nuxtjs/tailwindcss',
+  ],
   eslint: {
     // 
-  }
+  },
+  pinia: {
+    storesDirs: ['./stores/**'],
+  },
 })
