2b619c99c52e5ba76044c68ce805c5843dbe3073
Robin Oosterik <robin.oosterik@pescheck.nl>
2024-12-05 01:05:39 +0100

    removed deprecated lines

--- utils/db.js
+++ utils/db.js
@@ -4,8 +4,6 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
     console.log('Connected to MongoDB');
   } catch (error) {
