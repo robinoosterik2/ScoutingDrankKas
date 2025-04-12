8be4103fab4b667f5bfbde4ed977230167008c95
Robin Oosterik <robin.oosterik@pescheck.nl>
2025-04-12 13:32:36 +0200

    last uri updated aswell

--- utils/db.js
+++ utils/db.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {});
+    console.log(process.env.MONGODB_URI, 'process.env.MONGODB_URI')
+    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017', {});
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
